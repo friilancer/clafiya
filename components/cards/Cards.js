277233ca4b2b6cac4c792d5ad5c5ea082e3cd32b
friilancer <jirapzo7@gmail.com>
2021-11-17 19:50:53 +0100

    Feature: Secondary Navigation

---
+++ components/cards/Cards.js
@@ -0,0 +1,11 @@
+import styles from "../../styles/Cards.module.css"
+
+const Cards = () => {
+    return (
+        <div>
+            Cards here..holla at your boy
+        </div>
+    )
+}
+
+export default Cards
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,4 +1,7 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import Cards from '../../components/cards/Cards'
+import styles from '../../styles/dashboard.module.css'
 
 const Dashboard = () => {
     return(
@@ -6,8 +9,43 @@ const Dashboard = () => {
             <Head>
                 <title>Clafiya -Dashboard</title>
             </Head>
-            <main>
-                <h1>This is a dashboard</h1>
+            <main className={`${styles.container}`}>
+                <nav className={`${styles.nav__secondary}`}>
+                    <div className={`${styles.inputGroup} ${styles.nav_item}`}>
+                        <Image
+                            src="/images/ico-search.png" 
+                            alt="search"
+                            width={13}
+                            height={13}
+                            className={`${styles.inputGroup_icon}`}
+                        />
+                        <input 
+                            type="text" 
+                            placeholder="Search" 
+                            className={`${styles.inputGroup_input}`}
+                        />
+                    </div>
+                    <div className={`${styles.notificationGroup} ${styles.nav_item}`}>
+                        <Image
+                            src="/images/ico-notification.png" 
+                            alt="search"
+                            width={24}
+                            height={24}
+                            className={`${styles.notificationGroup_icon}`}
+                        />
+                        <div className={`${styles.notificationGroup_count}`}>
+                            9
+                        </div>
+                    </div>
+                    <Image
+                        src="/images/img-avatar.png" 
+                        alt="avatar"
+                        width={33}
+                        height={33}
+                        className={`${styles.inputGroup_icon} ${styles.nav_item}`}
+                    />                    
+                </nav>
+                <Cards />
             </main>
         </>
     )
